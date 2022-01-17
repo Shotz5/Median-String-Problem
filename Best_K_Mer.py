42a68f7dac7d6f9d144dd6a2bd4287b7cacea8db
Shotz5 <kitsulalex@gmail.com>
2022-01-16 20:39:38 -0800

    Added file input and error handling

--- Best_K_Mer.py
+++ Best_K_Mer.py
@@ -2,3 +2,9 @@ class Best_K_Mer:
     def __init__(self, dna, distance):
         self.dna = dna
         self.distance = distance
+
+    def isNull(self):
+        if self.dna == "" or self.distance == -1:
+            return 1
+        else:
+            return 0
--- MedianStringProblem.py
+++ MedianStringProblem.py
@@ -1,26 +1,38 @@
 import re
 import itertools as it
-
 from Best_K_Mer import Best_K_Mer
 
 def main():
     # Pull in file data
-    input_1 = open("input_3.txt", "r")
-    k = int(input_1.readline())
-    dna_string = input_1.readline()
-    dna_list = re.split("\s", dna_string)
+    file_name = input("Enter file name to be evaluated (including file extension): ")
+    
+    with open(file_name, "r") as input_1:
+        try:
+            k = int(input_1.readline())
+        except:
+            print("First line of file does not contain a parseable integer, please check file and try again.")
+            exit()
 
-    #Have k-mer and list of DNA, now need to iterate through all possible patterns of length k
-    letters = ["A", "C", "G", "T"]
-    best_k_mer = Best_K_Mer("", -1)
+        dna_string = input_1.readline()
+        dna_list = re.split("\s", dna_string)
 
-    for k_mer in it.product(letters, repeat=k):
-        distance = d(k_mer, dna_list)
-        if distance < best_k_mer.distance or best_k_mer.distance == -1:
-            best_k_mer = Best_K_Mer(k_mer, distance)
+        #Have k-mer and list of DNA, now need to iterate through all possible patterns of length k
+        letters = ["A", "C", "G", "T"]
+        best_k_mer = Best_K_Mer("", -1)
 
-    print(best_k_mer.dna)
+        # Iterates through all possible strings of length k using the letters in "letters"
+        for k_mer in it.product(letters, repeat=k):
+            distance = d(k_mer, dna_list)
+            if distance < best_k_mer.distance or best_k_mer.distance == -1:
+                best_k_mer = Best_K_Mer(k_mer, distance)
 
+        # Error handling in case the best_k_mer doesn't update
+        if best_k_mer.isNull():
+            print("Something went wrong, Best_K_Mer was not updated.")
+        else:
+            print(best_k_mer.dna)
+
+# Method that calculates the minimum distance for each DNA portion, and returns the sum of the distances
 def d(k_mer, dna):
     total_distance = 0
     for dna_portion in dna:
@@ -34,9 +46,14 @@ def d(k_mer, dna):
         if current_distance < current_lowest_distance or current_lowest_distance == -1:
             current_lowest_distance = current_distance
 
-        total_distance += current_lowest_distance
-    return total_distance
+        # Error handling for if current_lowest_distance doesn't update
+        if current_lowest_distance < 0:
+            print("Something went wrong, exiting...")
+            exit()
 
+        total_distance += current_lowest_distance
+
+    return total_distance
 
 if __name__ == "__main__":
     main()
